e3e97b0ae6a795606b63c26ea63879335280aee4
Andrew Cholakian <andrew@andrewvc.com>
2010-03-29 10:06:25 -0700

    Fixed security vuln where users could use /../ to access files outside of the webroot

--- example/basic.js
+++ example/basic.js
@@ -9,25 +9,25 @@ var
 
 http.createServer(function(req, res) {
   var ip = req.connection.remoteAddress;
-  paperboy
-    .deliver(WEBROOT, req, res)
-    .before(function() {
+  paperboy.deliver(WEBROOT, req, res, {
+    before: function() {
       sys.log('Recieved Request')
-    })
-    .after(function(statCode) {
+    },
+    after: function(statCode) {
       res.write('Delivered: '+req.url);
       log(statCode, req.url, ip);
-    })
-    .error(function() {
-      sys.puts('Error delivering: '+req.url);
-    })
-    .otherwise(function() {
+    },
+    error: function(statCode,msg) {
+      log(statCode, req.url, ip, msg)
+    },
+    otherwise: function() {
       var statCode = 404;
       res.writeHeader(statCode, {'Content-Type': 'text/plain'});
       res.write('Sorry, no paper this morning!');
       res.close();
       log(statCode, req.url, ip);
-    });
+    }
+  });
 }).listen(PORT);
 
 function log(statCode, url, ip,err) {

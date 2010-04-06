1607b9a87d6bbff6afb1e70a1445605bebd8eb22
Andrew Cholakian <andrew@andrewvc.com>
2010-04-06 10:38:59 -0700

    Switched back to using delegates for main interface

--- example/basic.js
+++ example/basic.js
@@ -9,29 +9,27 @@ var
 
 http.createServer(function(req, res) {
   var ip = req.connection.remoteAddress;
-  paperboy.deliver(WEBROOT, req, res,
-  {
-    expires: 900 //Time in seconds frow now
-  },
-  {
-    before: function() {
+  paperboy
+    .deliver(WEBROOT, req, res)
+    .before(function() {
       sys.log('Recieved Request')
-    },
-    after: function(statCode) {
+    })
+    .after(function(statCode) {
       res.write('Delivered: '+req.url);
       log(statCode, req.url, ip);
-    },
-    error: function(statCode,msg) {
-      log(statCode, req.url, ip, msg);
-      res.close;
-    },
-    otherwise: function(err) {
-      var statCode = 404;
-      res.writeHeader(statCode, {'Content-Type': 'text/plain'});
+    })
+    .error(function(statCode,msg) {
+      res.sendHeader(statCode, {'Content-Type': 'text/plain'});
+      res.writeHead("Error: " + statCode);
       res.close();
+      log(statCode, req.url, ip, msg);
+    })
+    .otherwise(function(err) {
+      var statCode = 404;
+      res.writeHead(statCode, {'Content-Type': 'text/plain'});
+      res.close("Not Found");
       log(statCode, req.url, ip, err);
-    }
-  });
+    });
 }).listen(PORT);
 
 function log(statCode, url, ip,err) {
